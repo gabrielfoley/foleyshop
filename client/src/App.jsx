7dec691ebc3a5c1c2e5ef9ea6b17020867b9e1e5
gabrielfoley <80079989+gabrielfoley@users.noreply.github.com>
2022-12-27 17:29:35 -0800

    sync

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,27 @@
 import Home from "./pages/Home";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Cart from "./pages/Cart";
+
+import { Route, Routes, Navigate, Link } from "react-router-dom";
 
 const App = () => {
-  return <Home/>;
+  const user = true
+  return (
+<Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/products/:category" element={<Product/>}/>
+      <Route path="/product/:id" element={<ProductList/>}/>
+      <Route path="/cart" element={<Cart/>}/>
+      <Route path="/login" element={user ? <Navigate to="/"/> : <Login/>}/>
+      <Route path="/register" element={user ? <Navigate to="/"/> : <Register/>}/>
+      
+      
+</Routes>
+)
 };
+
 
 export default App;
